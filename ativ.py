cd8c44f941c097cfb231ebf1573458bd6080bb1d
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-04 14:52:39 -0300

    soma desses numeros

---
+++ ativ.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+num1 = int(input("digite o primeiro valor"))
+num2 = int(input("digite o segundo valor"))
+soma = num1+num2
+print(f"a soma é {num1} + {num2} = {soma}")
+
+
+
+
